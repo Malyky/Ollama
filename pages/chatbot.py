4a32feae9bd7a37b63cd1f9052786b37d0df7577
Martin Ly-Ky <martin.ly-ky@gmx.de>
2024-04-06 16:02:19 +0200

    Chatbot without langchain

---
+++ pages/chatbot.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+import streamlit as st
+
+st.title("ChatGPT-like clone with local Ollama :)")
+
+client = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "llama2"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="llama2",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
