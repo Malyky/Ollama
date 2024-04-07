1e0514392be9550a48ad19e528f75e7590e0c1b2
Martin Ly-Ky <martin.ly-ky@gmx.de>
2024-04-07 16:32:55 +0200

    Add Expander and openai to chatbot

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -1,28 +1,56 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+
 import streamlit as st
+import os
 
-st.title("ChatGPT-like clone with local Ollama :)")
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+st.title("🦙💬 Ollama Chatbot")
+model_options = ["llama2", "gpt-3.5-turbo"]
 
-client = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1")
+# Create a box for model configurations
+with st.expander("Model Configurations"):
+    # Add a slider for temperature
+    selected_model = st.selectbox('Choose a model', model_options, key='selected_model')
+    temperature = st.slider("Temperature", min_value=0.1, max_value=2.0, value=1.0, step=0.1, format="%.1f", key='temperature')
+    top_p = st.slider('top_p', min_value=0.01, max_value=1.0, value=1.0, step=0.01, key='top_p')
+    max_length = st.slider('max_length', min_value=1, max_value=256, value=120, step=8, key='max_length')
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "llama2"
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.button('Clear Chat History', on_click=clear_chat_history)
+
+if "selected_model" not in st.session_state:
+    st.session_state["selected_model"] = "llama2"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+base_url = "http://localhost:11434/v1"
+if selected_model == "llama2":
+    base_url = "http://localhost:11434/v1"
+elif selected_model == "gpt-3.5-turbo":
+    base_url = "https://api.openai.com/v1"
+else:
+    base_url = "http://localhost:11434/v1"
+
+client = OpenAI(api_key=api_key, base_url=base_url)
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
+        #with st.spinner("Thinking..."):
         stream = client.chat.completions.create(
-            model="llama2",
+            model=st.session_state["selected_model"],
+            max_tokens=st.session_state["max_length"],
+            top_p=st.session_state["top_p"],
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
