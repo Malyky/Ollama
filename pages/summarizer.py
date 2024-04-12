870c091d5ec207b7d201718a1429e4ed3e7e5efa
Martin Ly-Ky <martin.ly-ky@gmx.de>
2024-04-12 16:32:50 +0200

    Add Summary Class and String parser

--- pages/summarizer.py
+++ pages/summarizer.py
@@ -13,12 +13,19 @@ from apikey import apikey
 import os
 import tempfile
 from streamlit_option_menu import option_menu
+from typing import Iterable
+from langchain_core.messages import AIMessage, AIMessageChunk
+from langchain_core.runnables import RunnableGenerator
 
 #with st.sidebar:
 #    selected = option_menu("Main Menu", ["ChatGeneration", 'SummarizePdfs', 'SummarizeWordFiles'],
 #                           icons=['house', 'gear'], menu_icon="cast", default_index=1)
 #    selected
 
+class Summary:
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
 from menu import menu_with_redirect
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
@@ -32,6 +39,20 @@ llm = Ollama(model="llama2",
              num_gpu=32,
              )
 
+
+def parse(ai_message: dict) -> str:
+    """Parse the AI message."""
+    return ai_message["output_text"]
+
+
+
+def streaming_parse(chunks: Iterable[AIMessageChunk]) -> Iterable[str]:
+    for chunk in chunks:
+        yield chunk.content.swapcase()
+
+
+streaming_parse = RunnableGenerator(streaming_parse)
+
 def summarize_pdfs_from_folder(pdfs_folder):
     summaries = []
     for pdf_file in pdfs_folder:
@@ -43,7 +64,7 @@ def summarize_pdfs_from_folder(pdfs_folder):
         docs = loader.load_and_split()
         chain = load_summarize_chain(llm, chain_type="map_reduce")
         summary = chain.run(docs)
-        summaries.append(summary)
+        summaries.append((summary, pdf_file.name))
 
         # Delete the temporary file
         os.remove(temp_path)
@@ -59,9 +80,9 @@ def summarizeWordFiles(wordFiles):
 
         loader: Docx2txtLoader = Docx2txtLoader(temp_path)
         docs = loader.load_and_split()
-        chain = load_summarize_chain(llm, chain_type="map_reduce")
-        summary = chain.run(docs)
-        summaries.append(summary)
+        chain = load_summarize_chain(llm, chain_type="map_reduce") | parse
+        summary = chain.invoke(docs)
+        summaries.append(Summary(wordFile.name, summary))
 
         # Delete the temporary file
         os.remove(temp_path)
@@ -69,16 +90,16 @@ def summarizeWordFiles(wordFiles):
     return summaries
 
 # Streamlit App
-st.title("Multiple PDF Summarizer")
+st.title("Multiple Documents Summarizer")
 
 # Allow user to upload PDF files
 pdf_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
-wordFiles = st.file_uploader("Upload Wird files", type="docx", accept_multiple_files=True)
+wordFiles = st.file_uploader("Upload Word files", type="docx", accept_multiple_files=True)
 
 if pdf_files:
     # Generate summaries when the "Generate Summary" button is clicked
     if st.button("Generate Summary"):
-        st.write("Summaries:")
+        st.subheader("Summaries:")
         summaries = summarize_pdfs_from_folder(pdf_files)
         for i, summary in enumerate(summaries):
             st.write(f"Summary for PDF {i+1}:")
@@ -86,8 +107,9 @@ if pdf_files:
 if wordFiles:
     # Generate summaries when the "Generate Summary" button is clicked
     if st.button("Generate Summary"):
-        st.write("Summaries:")
+        st.subheader("Summaries:")
         summaries = summarizeWordFiles(wordFiles)
         for i, summary in enumerate(summaries):
-            st.write(f"Summary for Word {i+1}:")
-            st.write(summary)
+            st.write(f"Summary for{summary.name} :")
+            st.write(summary.text)
+
